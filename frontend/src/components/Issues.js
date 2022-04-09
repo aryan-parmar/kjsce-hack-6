9372a497408d00eac9e2b1f78e0a298cd8306fdc
Aryan <aaryan.p114@gmail.com>
2022-04-09 22:33:56 +0530

    issue page styling

--- frontend/src/components/Issues.js
+++ frontend/src/components/Issues.js
@@ -2,16 +2,16 @@ import React from 'react'
 import "./style/issue.css"
 export default function Issues() {
     return (
-        <div class="wrapper-issue">
+        <div className="wrapper-issue">
             <form action="">
                 <h1>Add an issue</h1>
                 <div id="wizard">
                     <section>
-                        <div class="form-row"> <input type="text" class="form-control" placeholder="Location" /> </div>
-                        <div class="form-row"> <input type="text" class="form-control" placeholder="Issue" /> </div>
+                        <div className="form-row"> <input type="text" className="form-control" placeholder="Location" /> </div>
+                        <div className="form-row"> <input type="text" className="form-control" placeholder="Issue" /> </div>
                     </section>
                     <section>
-                        <div class="form-row"> <textarea name="" id="" class="form-control" placeholder="Issue Description"></textarea> </div>
+                        <div className="form-row"> <textarea name="" id="" className="form-control" placeholder="Issue Description"></textarea> </div>
                         <div className='checkBox'>
                             <label htmlFor="Anonymous">Anonymous</label>
                             <input id="Anonymous" type="checkbox" />
