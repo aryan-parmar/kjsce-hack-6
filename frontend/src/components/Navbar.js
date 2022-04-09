9c6519af0e784656af26ff56806d43b29f9fafc0
HARSH <harshpanddey9324@gmail.com>
2022-04-09 23:18:32 +0530

    New navbar(by new designer)

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,40 +4,58 @@ import "./style/navbar.css"
 
 export default function Navbar() {
   return (
-    <nav className="menu-container">
-    {/* <!-- burger menu --> */}
-    <input type="checkbox" aria-label="Toggle menu" />
-    <span></span>
-    <span></span>
-    <span></span>
+    <section className="navbar">
+    <div className="container">
+        <h1 className="logo"><a href="index.html">Yugma</a></h1>
+        <nav>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="#my_skills">Add an issue</a></li>
+                <li><a href="#about_me">About us</a></li>
+            </ul>
 
-    {/* <!-- logo --> */}
-    <Link to="#" className="menu-logo">
-      <img
-        src="/Logo.png"
-        alt="LOgo" />
-    </Link>
 
-    {/* <!-- menu items --> */}
-    <div className="menu">
-      <ul>
-        
-
-        
-      </ul>
-      <ul>
-        <li>
-          <Link to="signup">
-            Sign-up
-          </Link>
-        </li>
-        <li>
-          <Link to="login">
-            Login
-          </Link>
-        </li>
-      </ul>
+        </nav>
     </div>
-  </nav>
+</section>
+
+  //   <nav className="menu-container">
+  //   {/* <!-- burger menu --> */}
+  //   <input type="checkbox" aria-label="Toggle menu" />
+  //   <span></span>
+  //   <span></span>
+  //   <span></span>
+
+  //   {/* <!-- logo --> */}
+  //   <Link to="#" className="menu-logo">
+  //     <img
+  //       src="/Logo.png"
+  //       alt="LOgo" />
+  //   </Link>
+
+  //   {/* <!-- menu items --> */}
+  //   <div className="menu">
+  //     <ul>
+        
+
+        
+  //     </ul>
+  //     <ul>
+  //       <li>
+  //         <Link to="signup">
+  //           Sign-up
+  //         </Link>
+  //       </li>
+  //       <li>
+  //         <Link to="login">
+  //           Login
+  //         </Link>
+  //       </li>
+  //     </ul>
+  //   </div>
+  // </nav>
+
+
+
   )
 }
